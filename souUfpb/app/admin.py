0728819cd1ebff20755e0d1ea8cf1917ccec65ab
Larissa Gomes <larissa.marinho@gmail.com>
2023-11-06 00:21:27 -0300

    update models

--- souUfpb/app/admin.py
+++ souUfpb/app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Question, Choice, Curso, Disciplina, RelacaoDisciplinas, Pergunta
+from .models import Curso, Disciplina, RelacaoDisciplinas, Pergunta, Area
 
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(Curso)
 admin.site.register(Disciplina)
 admin.site.register(RelacaoDisciplinas)
 admin.site.register(Pergunta)
+admin.site.register(Area)
--- souUfpb/app/models.py
+++ souUfpb/app/models.py
@@ -1,44 +1,49 @@
-from enum import auto
 from django.db import models
-import uuid
+from django.contrib.auth.models import User
 
+class Area(models.Model):
 
-class Base(models.Model):
-
-    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4())
-    created_at = models.DateField(auto_now=True)
-    updated_at = models.DateField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Question(Base):
-
-    question_text = models.CharField(max_length=200)
-    area = models.CharField(max_length=50, default="")
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.question_text
-
-class Choice(Base):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.CharField(
-        max_length=25,
-        choices=(
-            ('discordo_totalmente', '1'),
-            ('discordo_parcialmente', '2'),
-            ('neutro', '3'),
-            ('concordo_parcialmente', '4'),
-            ('concordo_totalmente', '5'),
-        )
-    )
+        return self.nome
     
+class Pergunta(models.Model):
+
+    pergunta = models.CharField(max_length=200)
+
+    exatas = models.IntegerField(default=0)
+    biologicas = models.IntegerField(default=0)
+    engenharias = models.IntegerField(default=0)
+    saude = models.IntegerField(default=0)
+    agrarias = models.IntegerField(default=0)
+    linguistica = models.IntegerField(default=0)
+    sociais = models.IntegerField(default=0)
+    humanas = models.IntegerField(default=0)
+
     def __str__(self):
-        return self.choice
+        return self.pergunta
 
-class Curso(Base):
+# class Escolha(models.Model):
+#     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+#     escolha = models.CharField(
+#         max_length=25,
+#         choices=(
+#             ('discordo_totalmente', '1'),
+#             ('discordo_parcialmente', '2'),
+#             ('neutro', '3'),
+#             ('concordo_parcialmente', '4'),
+#             ('concordo_totalmente', '5'),
+#         )
+#     )
+    
+#     def __str__(self):
+#         return self.escolha
 
+class Curso(models.Model):
+
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
     nome = models.CharField(max_length=128)
     numPeriodos = models.IntegerField()
     chMinima = models.IntegerField()
@@ -51,13 +56,11 @@ class Disciplina(models.Model):
     codigo = models.CharField(primary_key=True, max_length=32)
     nome = models.CharField(max_length=128)
     carga = models.IntegerField()
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.codigo
     
-class RelacaoDisciplinas(Base):
+class RelacaoDisciplinas(models.Model):
 
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
@@ -65,14 +68,16 @@ class RelacaoDisciplinas(Base):
 
     def __str__(self):
         return f'{self.curso.nome} - {self.periodo}º período - {self.disciplina.codigo}'
-    
-class Pergunta(Base):
-    descricao = models.CharField(max_length=250)
-    opcao_1 = models.CharField(default='Nada interessado', editable=False)
-    opcao_2 = models.CharField(default='Pouco interessado', editable=False)
-    opcao_3 = models.CharField(default='Neutro', editable=False)
-    opcao_4 = models.CharField(default='Interessado', editable=False)
-    opcao_5 = models.CharField(default='Muito interessado', editable=False)
 
-    def __str__(self):
-        return self.descricao
+    
+class Resultado(models.Model):
+
+    exatas = models.IntegerField(default=0)
+    biologicas = models.IntegerField(default=0)
+    engenharias = models.IntegerField(default=0)
+    saude = models.IntegerField(default=0)
+    agrarias = models.IntegerField(default=0)
+    linguistica = models.IntegerField(default=0)
+    sociais = models.IntegerField(default=0)
+    humanas = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
